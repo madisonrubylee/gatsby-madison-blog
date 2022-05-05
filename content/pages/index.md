35b33237ee2ed55cfd3224f91ec57331d9dd3584
madisonrubylee <lamantciel@naver.com>
2022-05-05 22:26:50 +0900

    init

---
+++ content/pages/index.md
@@ -0,0 +1,9 @@
+---
+template: 'index-template'
+---
+
+#반갑습니다. 
+#프론트엔드 개발자 
+#이현주입니다.
+
+A minimal & beautiful gatsby personal blog starter with nice glassmorphism UI.
---
+++ gatsby-config.js
@@ -0,0 +1,166 @@
+module.exports = {
+  siteMetadata: {
+    title: `madison blog`,
+    author: {
+      name: `Yinka Adedire`,
+      summary: `Self-taught front-end web dev. JAMStack.`,
+    },
+    openGraphImage: `open-graph-image.png`,
+    description: `A minimal & beautiful gatsby personal blog starter with a nice glassmorphism UI.`,
+    siteUrl: `https://gatsbyglass.netlify.app`,
+    social: {
+      twitter: `yinkakun`,
+    },
+    socialLinks: [
+      {
+        name: 'github',
+        url: 'https://github.com/madisonrubylee',
+      },
+      {
+        name: 'blog',
+        url: 'https://hoontinparis.tistory.com/',
+      },
+      {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/in/hyunjoo-l-0a53011a3/',
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `media`,
+        path: `${__dirname}/static/media`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/content/pages`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              staticFolderName: 'static',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/netlify-cms/index.js`,
+        enableIdentityWidget: true,
+        publicPath: 'admin',
+        htmlTitle: 'Content Manager',
+        includeRobots: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map((node) => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ 'content:encoded': node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Source Sans Pro`, `Poppins\:400,400i,700`],
+        display: 'swap',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Frosted Blog`,
+        short_name: `Gatsby Frosted`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        icon: `src/images/madison.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
+};
